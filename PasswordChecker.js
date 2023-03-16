aecb53ab3b0f2c249abcb91c90c77b93bf4da39f
Harikrashna <hparikh@argusoft.com>
2023-03-16 19:35:27 +0530

    added random function

--- PasswordChecker.js
+++ PasswordChecker.js
@@ -22,3 +22,8 @@ else
 {
     console.log('password must be longer');
 }
+function rollDie(){
+    let roll = Math.floor(Math.random() * 6) + 1 ;
+    console.log(`Rolled : ${roll}`);
+}
+rollDie();
